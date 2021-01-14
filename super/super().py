481b3a2b7d07fe6567784f2a7fadc40edf4021bd
zhouhuajian <test@zhouhuajian.com>
2021-01-14 16:01:34 +0800

    super内置函数+课程资料

--- super/super().py
+++ super/super().py
@@ -1,6 +1,57 @@
 # super([type[, object-or-type]])
+# super
+# super() -> same as super(__class__, <first argument>)
+#     super(type) -> unbound super object
+#     super(type, obj) -> bound super object; requires isinstance(obj, type)
+#     super(type, type2) -> bound super object; requires issubclass(type2, type)
+class B:
+    def test(self):
+        print("B test")
+class C(B):
+    def test(self):
+        print("C test")
+        # super().test()
+        super(C, self).test() # MRO 方法解析顺序
+        # self mro  C->B->object
+C().test()
+print('------')
+print(f'{ super(C) = }') # unbound super object
+print('-------')
+class B:
+    @classmethod
+    def test(cls):
+        print("B test")
+class C(B):
+    @classmethod
+    def test(cls):
+        print("C test")
+        # super().test()
+        # print(f'{ cls = }')
+        # super(type, type2) -> bound super object
+        super(C, C).test()
+C.test()
+# 它会将方法调用委托给 type 或兄弟类
+class A:
+    def test(self):
+        print("A test")
+class B(A):
+    def test(self):
+        print("B test")
+        print(f'{ self = }')
+        print(f'{ type(self).mro() = }')
+        super(B, self).test()
+        # self 是D类的实例 mro D->B->C->A->object
+        # C类是B的兄弟类，而不是父类
+class C(A):
+    def test(self):
+        print("C test")
+class D(B, C):
+    def test(self):
+        print("D test")
+        super(D, self).test()
+print('-------')
+D().test()
 # 返回一个代理对象，它会将方法调用委托给 type 的父类或兄弟类。 这对于访问已在类中被重载的继承方法很有用。
-#
 # object-or-type 确定用于搜索的 method resolution order。 搜索会从 type 之后的类开始。
 #
 # 举例来说，如果 object-or-type 的 __mro__ 为 D -> B -> C -> A -> object 并且 type 的值为 B，则 super() 将会搜索 C -> A -> object。
